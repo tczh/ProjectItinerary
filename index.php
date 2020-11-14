44d8fb73fad475016597aed2562ceb5c57e0419a
xiulingchua <51019711+xiulingchua@users.noreply.github.com>
2020-11-14 17:02:34 +0800

    navbar

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous"/>
 
     <script src="index.js"></script>
+    
 
     <title>Welcome To Tim's Travel Agent</title>
 
